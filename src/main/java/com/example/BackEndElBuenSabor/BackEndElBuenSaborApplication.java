d3ca279d17ce05b56c428b4cd88af75467452b5a
brunoSacchetti <bruno.sacchetti@alumnos.frm.utn.edu.ar>
2024-04-15 15:20:48 -0300

    first

---
+++ src/main/java/com/example/BackEndElBuenSabor/BackEndElBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndElBuenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndElBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndElBuenSaborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackEndElBuenSabor/BackEndElBuenSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndElBuenSabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndElBuenSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
